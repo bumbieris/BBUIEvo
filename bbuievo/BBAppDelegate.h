af1a303e7b3bf627a93e452a867c6296ed45f271
bumbieris <bumbierisbumbieris@bumbieriss-MacBook-Pro.local>
2013-03-26 03:21:02 +0000

    initial commit

---
+++ bbuievo/BBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBAppDelegate.h
+//  bbuievo
+//
+//  Created by bumbieris on 26/03/2013.
+//  Copyright (c) 2013 bumbieris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
