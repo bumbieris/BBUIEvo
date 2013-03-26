f9150b0494052b3b2cb0d84a172214bf3bb6911c
bumbieris <bumbierisbumbieris@bumbieriss-MacBook-Pro.local>
2013-03-26 04:01:41 +0000

    update files after testing

---
+++ bbuievo/BBTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBTestViewController.h
+//  bbuievo
+//
+//  Created by bumbieris on 26/03/2013.
+//  Copyright (c) 2013 bumbieris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBTestViewController : UIViewController
+
+@end
---
+++ bbuievo/BBTestViewController.m
@@ -0,0 +1,60 @@
+//
+//  BBTestViewController.m
+//  bbuievo
+//
+//  Created by bumbieris on 26/03/2013.
+//  Copyright (c) 2013 bumbieris. All rights reserved.
+//
+
+#import "BBTestViewController.h"
+#import "BBHyperLabel.h"
+
+@interface BBTestViewController ()
+@property (nonatomic, strong) IBOutlet BBHyperLabel* label;
+@property (nonatomic, strong) IBOutlet BBHyperLabel* labelNoUnderline;
+@end
+
+@implementation BBTestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    _label.target = self;
+    _label.action = @selector(linkClicked:);
+    _labelNoUnderline.target = self;
+    _labelNoUnderline.action = @selector(linkClicked:);
+    _labelNoUnderline.underline = FALSE;
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    [_label deselect];
+    [_labelNoUnderline deselect];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void) linkClicked: (id) a
+{
+    if ([self.navigationItem.title isEqualToString:@"Foo"]){
+        [self performSegueWithIdentifier:@"Foo" sender:self];
+    }else
+        [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
