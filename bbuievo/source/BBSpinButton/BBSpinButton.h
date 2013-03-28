1d15c031c4882e1eaa7514b4770fcfc537740962
bumbieris <bumbierisbumbieris@bumbieriss-MacBook-Pro.local>
2013-03-28 03:21:50 +0000

    new files

---
+++ bbuievo/source/BBSpinButton/BBSpinButton.h
@@ -0,0 +1,26 @@
+//
+//  BBSpinButton.h
+//  bbuievo
+//
+//  Created by bumbieris on 28/03/2013.
+//  Copyright (c) 2013 bumbieris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBSpinButton : UIControl
+@property (readonly, nonatomic) UILabel* label;
+@property (readonly, nonatomic) UIActivityIndicatorView* spinner;
+@property (assign, nonatomic) CGFloat spinnerXSpacing;
+@property (assign, nonatomic) BOOL active;
+@property (assign, nonatomic) NSString* text;
+@property (assign, nonatomic) UIFont* font;
+@property (strong, nonatomic) UIColor* textColor;
+@property (strong, nonatomic) UIColor* highlightColor;
+@property (strong, nonatomic) UIColor* highlightBkColor;
+@property (strong, nonatomic) UIColor* normalBkColor;
+
+- (void) showSpinner;
+- (void) hideSpinner;
+
+@end
